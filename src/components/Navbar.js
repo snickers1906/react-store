7ee3451e330982602cdf85300cbc0ad42db99f3f
sneaky <sneakers1906@gmail.com>
2019-04-27 23:31:36 +0200

    Added new components

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello froom Navbar</h3>
+            </div>
+        )
+    }
+}
